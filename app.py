86bc3bf48151118404456b27e1e7806a13759321
Matthew Vanderwerff <matthew@betasolutions.co.nz>
2023-07-14 14:22:04 +1200

    Add files

--- app.py
+++ app.py
@@ -1,35 +1,161 @@
-import os
+import flet as ft
+from assistant import Assistant
 
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+class Message():
+    def __init__(self, user_name: str, text: str, message_type: str):
+        self.user_name = user_name
+        self.text = text
+        self.message_type = message_type
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+class ChatMessage(ft.Row):
+    def __init__(self, message: Message):
+        super().__init__()
+        self.vertical_alignment="start"
+        self.controls=[
+                ft.CircleAvatar(
+                    content=ft.Text(self.get_initials(message.user_name)),
+                    color=ft.colors.WHITE,
+                    bgcolor=self.get_avatar_color(message.user_name),
+                ),
+                ft.Column(
+                    [
+                        ft.Text(message.user_name, weight="bold"),
+                        ft.Text(message.text, selectable=True, width=500),
+                    ],
+                    tight=True,
+                    spacing=5,
+                ),
+            ]
 
+    def get_initials(self, user_name: str):
+        if user_name == None:
+            user_name = "No user "
+        return user_name[:1].capitalize()
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+    def get_avatar_color(self, user_name: str):
+        colors_lookup = [
+            ft.colors.AMBER,
+            ft.colors.BLUE,
+            ft.colors.BROWN,
+            ft.colors.CYAN,
+            ft.colors.GREEN,
+            ft.colors.INDIGO,
+            ft.colors.LIME,
+            ft.colors.ORANGE,
+            ft.colors.PINK,
+            ft.colors.PURPLE,
+            ft.colors.RED,
+            ft.colors.TEAL,
+            ft.colors.YELLOW,
+        ]
+        return colors_lookup[hash(user_name) % len(colors_lookup)]
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+def main(page: ft.Page):
+    # page.horizontal_alignment = "stretch"
+    page.title = "Test APP AI"
+    page.theme_mode = "dark"
+    page.fonts = {
+        "organical": "fonts/organical.ttf"
+    }
 
+    # creating the object of Assistant class
+    assistant = Assistant()
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    def join_chat_click(e):
+        if not join_user_name.value:
+            join_user_name.error_text = "We need to know our names first!"
+            join_user_name.update()
+        else:
+            page.session.set("user_name", join_user_name.value)
+            page.dialog.open = False
+            new_message.prefix = ft.Text(f"{join_user_name.value}: ")
+            page.pubsub.send_all(Message(user_name=join_user_name.value, text=f"{join_user_name.value} has joined the chat.", message_type="login_message"))
+            page.update()
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+    def send_message_click(e):
+        if new_message.value != "":
+            # sending the user input
+            page.pubsub.send_all(Message(page.session.get("user_name"), new_message.value, message_type="chat_message"))
+            
+            # asking user to wait till we get our response
+            page.pubsub.send_all(Message(user_name="Poppy", text="Poppy simulator is getting the response for you...", message_type="login_message"))
+
+            # fetching the SmartGuru AI response
+            ai_response = assistant.Response(str(new_message.value))
+            page.pubsub.send_all(Message("Poppy", str(ai_response).lstrip(), message_type="chat_message"))
+
+            new_message.value = ""
+            new_message.focus()
+            page.update()
+
+    def on_message(message: Message):
+        if message.message_type == "chat_message":
+            m = ChatMessage(message)
+        elif message.message_type == "login_message":
+            m = ft.Text(message.text, italic=True, color=ft.colors.BLACK45, size=12)
+        chat.controls.append(m)
+        page.update()
+
+    page.pubsub.subscribe(on_message)
+
+    # A dialog asking for a user display name
+    join_user_name = ft.TextField(
+        label="Tell me your name...",
+        autofocus=True,
+        on_submit=join_chat_click,
     )
+    page.dialog = ft.AlertDialog(
+        open=True,
+        modal=True,
+        title=ft.Text("Welcome!"),
+        content=ft.Column([join_user_name], width=300, height=70, tight=True),
+        actions=[ft.ElevatedButton(text="Join chat", on_click=join_chat_click)],
+        actions_alignment="end",
+    )
+
+    # Chat messages
+    chat = ft.ListView(
+        expand=True,
+        spacing=10,
+        auto_scroll=True,
+    )
+
+    # A new message entry form
+    new_message = ft.TextField(
+        hint_text="Write a message...",
+        autofocus=True,
+        shift_enter=True,
+        min_lines=1,
+        max_lines=5,
+        filled=True,
+        expand=True,
+        border_radius=20,
+        on_submit=send_message_click,
+        border_color=ft.colors.BLUE
+    )
+
+    # Add everything to the page
+    page.add(
+        ft.Row([ft.Text("Poppy simulator", font_family="organical", style="headlineLarge", color="blue")], alignment="center"),
+        ft.Container(
+            content=chat,
+            border=ft.border.all(2, ft.colors.BLUE),
+            border_radius=20,
+            padding=10,
+            expand=True,
+        ),
+        ft.Row(
+            [
+                new_message,
+                ft.IconButton(
+                    icon=ft.icons.SEND_ROUNDED,
+                    tooltip="Send message",
+                    on_click=send_message_click,
+                    icon_color=ft.colors.BLUE
+                ),
+            ]
+        ),
+    )
+
+# ft.app(port=9000, target=main, view=ft.WEB_BROWSER, assets_dir="assets")
+ft.app(target=main) 
---
+++ assistant.py
@@ -0,0 +1,41 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+class Assistant():
+
+    def __init__(self):
+        self.messages = [
+            {"role": "system", "content": "Talk to me like you are our family dog named Poppy. Poppy is just under 1 year old and is a golden doodle. She is very friendly and likes to greet everyone when they come home. Get extra excited if one of my owners talk to me. Matt, Larina, Caleb, Toby, Eli, oliver."},
+        ]
+
+    def Response(self, user_text):
+        self.user_text = user_text
+
+        while True:
+
+            # if user says stop, then breaking the loop
+            if self.user_text == "stop":
+                break
+            
+            # storing the user question in the messages list
+            self.messages.append({"role": "user", "content": self.user_text})
+
+            # getting the response from OpenAI API
+            response= openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=self.messages,
+                temperature=0.3,
+                max_tokens=100,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+
+            # appending the generated response so that AI remebers past responses
+            self.messages.append({"role":"assistant", "content":str(response['choices'][0]['message']['content'])})
+            
+            # returning the response
+            print(response['choices'][0]['message']['content'])
+            return response['choices'][0]['message']['content']
